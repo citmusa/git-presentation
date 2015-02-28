d910796f35e3ee79815030f49a4a08026cf853d8
Citlalli Murillo <citmusa@gmail.com>
2015-02-27 22:43:56 -0600

    Initial commit

---
+++ js/touch.js
@@ -0,0 +1,5 @@
+if ("ontouchstart" in document.documentElement) {
+    document.querySelector(".default").className = "default hidden";
+    document.querySelector(".touch").className = "touch"
+}
+
